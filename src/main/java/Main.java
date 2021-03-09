1157418bd0f4d4b64a431a2cb5cb6600b6f0d1a0
IfrimAndrei <andrei_ifrim@yahoo.com>
2021-03-09 20:29:39 +0200

    v 1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,27 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main (String[] args){
-        var students = IntStream.rangeClosed(0, 3)
+        /*var students = IntStream.rangeClosed(0, 3)
                 .mapToObj(i -> new Student("S" + i) )
                 .toArray(Student[]::new);
 
         List<Student> studentList = new LinkedList<>( Arrays.asList( students ) );
-        studentList.sort( (Comparator.comparing( Student::getName )) );
+        studentList.sort( (Comparator.comparing( Student::getName )) );*/
 
         var schools = IntStream.rangeClosed(0, 2)
                 .mapToObj(i -> new School("H" + i) )
                 .toArray(School[]::new);
 
 
-        TreeSet<School> schoolList = new TreeSet<>();
-        for (School s : schools) {
-            schoolList.add(s);
-        }
+        Set<School> schoolList = new TreeSet<School>();
+        //for (School s : schools) {
+         //   schoolList.add(s);
+        //}
+        schoolList.add(new School( "Nume" ));
         //( Arrays.asList( schools )
 
 
-
-        System.out.println(studentList + "\n" + schoolList );
+        System.out.println(schools[1].compareTo( schools[1]));
+        //System.out.println(studentList + "\n" + schoolList );
     }
 }
