ad2f9193dc060d108c122b8044b8757f77ee65c5
IfrimAndrei <andrei_ifrim@yahoo.com>
2021-03-12 16:08:15 +0200

    Compulsory done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,16 +16,50 @@ public class Main {
         Set<School> schoolList = new TreeSet<School>( Arrays.asList( schools ) );
 
         Map <Student,List<School>> studPrefMap = new HashMap<>();
+        Map <School,List<Student>> schoolPrefMap = new HashMap<>();
+        initMaps(schoolPrefMap,studPrefMap,students,schools);
+        ShowPref( schoolPrefMap ,studPrefMap);
+
+    }
+    public static void initMaps(Map <School,List<Student>> schoolPrefMap,Map <Student,List<School>> studPrefMap,Student[] students,School[] schools)
+    {
         studPrefMap.put(students[0], Arrays.asList(schools[0], schools[1], schools[2]));
         studPrefMap.put(students[1], Arrays.asList(schools[0], schools[1], schools[2]));
         studPrefMap.put(students[2], Arrays.asList(schools[0], schools[1]));
         studPrefMap.put(students[3], Arrays.asList(schools[0], schools[2]));
 
-        Map <School,List<Student>> schoolPrefMap = new HashMap<>();
         schoolPrefMap.put(schools[0], Arrays.asList(students[3], students[0], students[1], students[2]));
-        schoolPrefMap.put(schools[0], Arrays.asList(students[0], students[2], students[1]));
-        schoolPrefMap.put(schools[0], Arrays.asList(students[0], students[1], students[3]));
+        schoolPrefMap.put(schools[1], Arrays.asList(students[0], students[2], students[1]));
+        schoolPrefMap.put(schools[2], Arrays.asList(students[0], students[1], students[3]));
 
 
     }
+    public static String studentsToString(List<Student> students)
+    {
+        StringBuilder studs= new StringBuilder();
+        for( Student i: students)
+        {
+            //System.out.println(i.getName() );
+            studs.append( i.getName() ).append( " " );
+        }
+        return studs.toString();
+    }
+    public static String schoolsToString(List<School> schools)
+    {
+        StringBuilder schoolString= new StringBuilder();
+        for( School i: schools)
+        {
+            schoolString.append( i.getName() ).append( " " );
+        }
+        return schoolString.toString();
+    }
+    public static void ShowPref(Map <School,List<Student>> schoolPrefMap,Map <Student,List<School>> studPrefMap)
+    {
+        System.out.println("Lista de preferinte este: ");
+        schoolPrefMap.forEach((key, value) -> System.out.println(key.getName() + ": " + studentsToString( value )));
+        System.out.println();
+        studPrefMap.forEach((key, value) -> System.out.println(key.getName() + ": " + schoolsToString( value )));
+    }
+
+
 }
